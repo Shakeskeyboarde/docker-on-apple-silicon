c6e2ce8800d98a7dde71295cd01362014bc96509
Chris Ackerman <chris@topher.land>
2021-02-10 05:25:35 -0800

    Fixed bash curl commands.

--- setup-local-docker-client.sh
+++ setup-local-docker-client.sh
@@ -1,31 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-if [ -z "$__SUDO_GUARD__" ]; then
-  __TEMP__=$(mktemp -d)
-  function finish() {
-    rm -rf "$__TEMP__"
-  }
-  trap finish EXIT
+__TEMP__=$(mktemp -d)
+function finish() {
+  rm -rf "$__TEMP__"
+}
+trap finish EXIT
 
-  __VERSION__=$(curl -s https://download.docker.com/mac/static/stable/x86_64/ | grep -oE "docker-[0-9.]+\.tgz" | tail -n 1 | grep -oE "[0-9.]+[0-9]")
-  echo "Downloading Docker CLI v$__VERSION__"
-  curl -so "$__TEMP__/docker-client.tar.gz" "https://download.docker.com/mac/static/stable/x86_64/docker-$__VERSION__.tgz"
-  tar xzf "$__TEMP__/docker-client.tar.gz" -C "$__TEMP__"
-  echo "Copying to '/usr/local/bin/docker'"
-fi
+__VERSION__=$(curl -s https://download.docker.com/mac/static/stable/x86_64/ | grep -oE "docker-[0-9.]+\.tgz" | tail -n 1 | grep -oE "[0-9.]+[0-9]")
+echo "Downloading Docker CLI v$__VERSION__"
+curl -so "$__TEMP__/docker-client.tar.gz" "https://download.docker.com/mac/static/stable/x86_64/docker-$__VERSION__.tgz"
+tar xzf "$__TEMP__/docker-client.tar.gz" -C "$__TEMP__"
 
-if [ $EUID != 0 ]; then
-  sudo __TEMP__="$__TEMP__" __SUDO_GUARD__=1 "$0" "$@" || exit $?
-fi
-
-if [ $EUID == 0 ]; then
-  cp -f "$__TEMP__/docker/docker" /usr/local/bin/
-
-  if [ ! -z "$__SUDO_GUARD__" ]; then
-    exit
-  fi
-fi
+echo "Copying to '/usr/local/bin/docker'"
+sudo cp -f "$__TEMP__/docker/docker" /usr/local/bin/
 
 echo "$(tput bold)$(tput setaf 2)Done.$(tput sgr0)"
 
